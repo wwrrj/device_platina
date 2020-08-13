c33e9ceaed488a3e7c950906d1d888e97a6e10f6
Hanif Ardhani <hanif.ardhani98@gmail.com>
2020-08-13 09:52:35 +0700

    platina: Add inherit MIUI Camera

--- aicp_platina.mk
+++ aicp_platina.mk
@@ -28,6 +28,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/platina/device.mk)
+$(call inherit-product, vendor/xiaomi/MiuiCamera/config.mk)
 
 # Inherit some common AICP stuff.
 $(call inherit-product, vendor/aicp/config/common_full_phone.mk)
